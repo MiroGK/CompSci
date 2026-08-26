71a03ad5da4b5961efbd60c31b4d471f3aa27955
MiroGK <mirokatan13@gmail.com>
2026-08-26 08:59:36 -0700

    First day of class

---
+++ Day0/SetupCheck.java
@@ -0,0 +1,9 @@
+package Day0; // this says it is inside the "Day 0" folder
+
+// first thing you do in Java is make a PUBLIC CLASS!1111
+
+public class SetupCheck {
+    public static void main(String[] args) {
+        System.out.println("Name: Miro Katan \nPhrase: ACS setup complete");
+    }
+}
