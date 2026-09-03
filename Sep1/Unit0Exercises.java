8c615623f4809e077b3b19657f03a92b7e3bf180
MiroGK <mirokatan13@gmail.com>
2026-09-03 08:53:22 -0700

    0.3 and 0.2 edits

--- Sep1/Unit0Exercises.java
+++ Sep1/Unit0Exercises.java
@@ -1,6 +1,8 @@
+package Sep1;
+
 public class Unit0Exercises {
     public static void main(String[] args) {
-        System.out.println("--- Variable Snapshot");
+        System.out.println("--- Variable Snapshot ---");
         int score = 85;
         double price = 19.99;
         boolean isPassing = true; 
@@ -9,6 +11,6 @@ public class Unit0Exercises {
         score = 90;
         price = 22.49;
         isPassing = false;
-        System.out.println("Score:"+score+"\nPrice: "+price+"\nPassing: "+isPassing);
+        System.out.println("Score: "+score+"\nPrice: "+price+"\nPassing: "+isPassing);
     }
 }
